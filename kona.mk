cad7e9d30558e2d7c97eb8650eeeec8a6956d6b7
Gabor Meszaros <meszaros.gabor.10k@gmail.com>
2025-03-05 13:33:35 +0100

    kona: dex2oat optimizations

--- kona.mk
+++ kona.mk
@@ -406,5 +406,15 @@ PRODUCT_PACKAGES += \
 PRODUCT_BOOT_JARS += \
     WfdCommon
 
+PRODUCT_VENDOR_PROPERTIES += \
+	pm.dexopt.ab-ota=speed-profile \
+	dalvik.vm.dexopt.secondary=true \
+	ro.vendor.qti.am.reschedule_service=true \
+	pm.dexopt.install=speed-profile \
+	ro.sys.fw.dex2oat_thread_count=8 \
+	dalvik.vm.dex2oat-threads=4 \
+	dalvik.vm.image-dex2oat-threads=4
+
+
 # Inherit the proprietary files
 $(call inherit-product, vendor/xiaomi/sm8250-common/sm8250-common-vendor.mk)
