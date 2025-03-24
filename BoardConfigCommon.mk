044f4dcf9869214c553ce5d0f822a9ea0f0d5b28
Gabor Meszaros <meszaros.gabor.10k@gmail.com>
2025-03-24 10:09:49 +0100

    Enable IEEE 802.11ax support

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -236,6 +236,9 @@ DEVICE_MATRIX_FILE += hardware/qcom-caf/common/compatibility_matrix.xml
 ODM_MANIFEST_SKUS += nfc
 ODM_MANIFEST_NFC_FILES := $(COMMON_PATH)/manifest_nfc.xml
 
+# Enable IEEE 802.11ax support
+CONFIG_IEEE80211AX := true
+
 # Wi-Fi
 BOARD_WLAN_DEVICE := qcwcn
 BOARD_HOSTAPD_DRIVER := NL80211
