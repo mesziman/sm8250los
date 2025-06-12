66b0adf2b94eeedbbe5cf2687faba4ac67d1b076
Sebastiano Barezzi <seba@sebaubuntu.dev>
2025-06-12 16:01:43 +0200

    sm8250-common: Convert overlays to RRO
    Change-Id: Id0226559b81e115a6340591e7a107d25d7ad6848
    
    # Conflicts:
    #	kona.mk
    #	rro_overlays/LineageSettingsOverlayCommon/res/values/config.xml

--- kona.mk
+++ kona.mk
@@ -293,12 +293,19 @@ PRODUCT_PACKAGES += \
     libstagefright_softomx_plugin.vendor
 
 # Overlays
-DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+PRODUCT_PACKAGES += \
+    CarrierConfigOverlayCommon \
+    FrameworkResOverlayCommon \
+    LineageDialerOverlayCommon \
+    LineageSDKOverlayCommon \
+    LineageSettingsOverlayCommon \
+    SettingsOverlayCommon \
+    SettingsProviderOverlayCommon \
+    SystemUIOverlayCommon \
+    TelephonyOverlayCommon \
+    WifiResourcesOverlayCommon
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
-PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += $(LOCAL_PATH)/overlay/packages/apps/CarrierConfig
 
 # Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
@@ -398,7 +405,7 @@ PRODUCT_PACKAGES += \
     hostapd \
     libwifi-hal-ctrl \
     libwifi-hal-qcom \
-    WifiResCommon \
+    NcmTetheringOverlay \
     wpa_supplicant \
     wpa_supplicant.conf
 
