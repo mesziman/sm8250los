cff55e191017321f768ab2d9826893c76e05700e
basamaryan <basam.aryan@gmail.com>
2025-07-08 15:51:28 -0400

    sm8250-common: Override kernel BPF version
    Change-Id: I3c581f3120b6258b3b45020c5ba12f6bf5b0ee27

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -155,6 +155,7 @@ BOARD_USES_QCOM_HARDWARE := true
 
 # Properties
 TARGET_ODM_PROP += $(COMMON_PATH)/odm.prop
+TARGET_PRODUCT_PROP += $(COMMON_PATH)/product.prop
 TARGET_SYSTEM_PROP += $(COMMON_PATH)/system.prop
 TARGET_VENDOR_PROP += $(COMMON_PATH)/vendor.prop
 ifneq ($(TARGET_IS_TABLET),true)
