391fd4d7df5df9dbd6c9fe549cacf60119aec31b
Gabor Meszaros <meszaros.gabor.10k@gmail.com>
2025-09-25 08:47:33 +0200

    build stagefright softomx

--- kona.mk
+++ kona.mk
@@ -279,7 +279,8 @@ PRODUCT_PACKAGES += \
     libOmxCore \
     libOmxVdec \
     libOmxVenc \
-    libstagefrighthw
+    libstagefrighthw \
+    libstagefright_softomx_plugin.vendor
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
