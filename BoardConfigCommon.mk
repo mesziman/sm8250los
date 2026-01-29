e4cef0c4c31a23d2f0d1cc7c73143f3e9f2c6092
Gabor Meszaros <meszaros.gabor.10k@gmail.com>
2026-01-29 10:23:55 +0100

    DNM: clang version to 20

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -104,6 +104,8 @@ TARGET_KERNEL_CONFIG := \
     vendor/debugfs.config \
     vendor/xiaomi/sm8250-common.config
 
+TARGET_KERNEL_CLANG_VERSION := r547379
+#r563880c
 # Partitions
 BOARD_FLASH_BLOCK_SIZE := 262144 # (BOARD_KERNEL_PAGESIZE * 64)
 ifeq ($(TARGET_IS_VAB),true)
